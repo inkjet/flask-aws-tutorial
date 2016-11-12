2a48fcab926303d4b0983031ed1b3b75b0c8e887
Ubuntu <ubuntu@ip-172-31-13-18.ec2.internal>
2016-11-12 17:14:10 +0000

    new file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:1Pedromachuca@test.ccpmc8m0irwr.us-east-1.rds.amazonaws.com:3306/test'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
