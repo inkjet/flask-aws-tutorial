4359c1d2dbbbcc1e4390157f5ad7acbfaceed5c3
Karana316 <kelseyrlandon@gmail.com>
2021-11-28 16:09:56 -0600

    testing

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
-application = Flask(__name__)
+application = app = Flask(__name__)
 application.config.from_object('config')
 bootstrap = Bootstrap(application)
 db = SQLAlchemy(application)
