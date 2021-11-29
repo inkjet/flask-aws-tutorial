683e7893aba6475867a25f10fece09307f7f4274
Karana316 <kelseyrlandon@gmail.com>
2021-11-29 13:37:58 -0600

    extra env config and testing

--- config.py
+++ config.py
@@ -4,12 +4,8 @@ from application.database import AWSPostgreSQL
 env = Env()
 env.read_env()
 
-
-# Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'postgresql://'+env.str("LOCAL_USER")+':'+env.str("LOCAL_PW")+'@localhost:5432/flask_db'
-
-SQLALCHEMY_DATABASE_URI = AWSPostgreSQL().uri
-
+LOCAL_DATABASE_URI = 'postgresql://'+env.str("LOCAL_USER")+':'+env.str("LOCAL_PW")+'@localhost:5432/flask_db'
+SQLALCHEMY_DATABASE_URI = (lambda x: LOCAL_DATABASE_URI if x else AWSPostgreSQL().uri)(env.bool("LOCAL_DEVELOPMENT"))
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
