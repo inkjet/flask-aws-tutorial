c159d3e1358a2a12a4998946862a3065193d5469
Karana316 <kelseyrlandon@gmail.com>
2021-11-29 19:08:18 -0600

    initial migration test

--- application/__init__.py
+++ application/__init__.py
@@ -12,9 +12,6 @@ bootstrap = Bootstrap(application)
 db.init_app(application)
 migrate.init_app(application, db)
 
-with application.app_context():
-    db.create_all()
-
 # this has to be relative or it overwrites the Flask application object/variable with the module itself
 # it also has to be last because the app has to be created before the decorator in views.py can register the views.
 from . import views
